34a7f066245b25eac303dfba3ee7f8ea12ff628e
daltva <85560253+daltva@users.noreply.github.com>
2023-03-27 22:45:41 -0400

    Added in animation and movement for knight. Refactored some code.

--- main.cpp
+++ main.cpp
@@ -17,11 +17,21 @@ int main(int argc, char const *argv[])
     //speed variable
     float speed{4.0};
 
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
     Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
     Vector2 knightPos{
         windowWidth/2.0f - 4.0f * (0.5f * (float)knight.width/6.0f),
         windowHeight/2.0f - 4.0f * (0.5f * (float)knight.height)
     };
+    // 1 : facing right, -1 : facing left
+    float rightLeft{1.f};
+
+    // animation variables
+    float runningTime{};
+    int frame{};
+    const int maxFrames {6};
+    const float updateTime{1.f/12.f};
 
     SetTargetFPS(60);
 
@@ -36,18 +46,33 @@ int main(int argc, char const *argv[])
         if (IsKeyDown(KEY_D)) direction.x += 1.0;
         if (IsKeyDown(KEY_W)) direction.y -= 1.0;
         if (IsKeyDown(KEY_S)) direction.y += 1.0;
-        if (Vector2Length(direction) != 0.0);
+        if (Vector2Length(direction) != 0.0)
         {
             // set mapPos = mapPos - direction
             Vector2Normalize(direction);
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            knight = knight_run;
+        }
+        else
+        {
+            knight = knight_idle; 
         }
 
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
+        //update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrames) frame = 0;
+        }
+
         // draw the character
-        Rectangle source{0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
         Rectangle dest{knightPos.x, knightPos.y, 4.0f * (float)knight.width/6.0f, 4.0f * (float)knight.height};
         DrawTexturePro (knight, source, dest, Vector2{}, 0.f, WHITE); 
 
