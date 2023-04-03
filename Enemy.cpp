d7f1f4a2bce14c290d326c6e5d1e2e6e14d188db
daltva <85560253+daltva@users.noreply.github.com>
2023-04-02 23:19:13 -0400

    Moved members and methods into BaseCharacter. Made variables protected.

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,13 +1,11 @@
 #include "Enemy.h"
-#include "raylib.h"
 
-Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) :
-    worldPos(pos),
-    texture(idle_texture),
-    idle(idle_texture),
-    run(run_texture)
-
+Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
     {
+        worldPos = pos;
+        texture = idle_texture;
+        idle = idle_texture;
+        run = run_texture;
         width = texture.width / maxFrames;
         height = texture.height; 
     }
@@ -30,19 +28,4 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture) :
     Rectangle source{frame * width, 0.f, rightLeft * width, height};
     Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
     DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-    }
-
-    void Enemy::undoMovement()
-    {
-        worldPosLastFrame = worldPos;
-    }
-
-    Rectangle Enemy::getCollisionRec()
-    {
-        return Rectangle{
-        screenPos.x,
-        screenPos.y,
-        width * scale,
-        height * scale
-    };
     }
