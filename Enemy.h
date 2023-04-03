6b5788dfb96ab5aef726d772a259fc44414d7d82
daltva <85560253+daltva@users.noreply.github.com>
2023-04-02 23:04:10 -0400

    Created BaseCharacter class for class inheritance.

--- Enemy.h
+++ Enemy.h
@@ -1,5 +1,7 @@
 #include "raylib.h"
-class Enemy
+#include "BaseCharacter.h"
+
+class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
